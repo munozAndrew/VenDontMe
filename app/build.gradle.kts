c6634b5f0e4e5abe92a73fa2f7f4778ad3e9ef53
Andrew <arvizumunozandrew@gmail.com>
2025-11-01 12:24:44 -0700

    Supabase SDK Added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 
-    kotlin("plugin.serialization") version "$2.0.21"
+    kotlin("plugin.serialization") version "2.0.21"
 }
 
 android {
